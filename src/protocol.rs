ef677a2a120e3d8bd02349cc68834944158f6546
Shogo Sawai <shogo.sawai@gmail.com>
2023-07-20 16:14:44 +0900

    wip

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub enum Message {
     ClientHello {
         domain: Option<String>,
@@ -24,4 +24,37 @@ pub enum Message {
     Shutdown {
         message: Option<String>,
     },
+}
+
+impl std::fmt::Debug for Message {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Message::ClientHello {
+                domain,
+                path,
+                auth_token: _,
+            } => {
+                write!(
+                    f,
+                    "Message::ClientHello domain={:?}, path={:?}",
+                    domain, path
+                )
+            }
+            Message::ServerHello { domain, path } => {
+                write!(f, "Message::ServerHello domain={}, path={}", domain, path)
+            }
+            Message::NewConnection { id } => {
+                write!(f, "Message::NewConnection id={}", id)
+            }
+            Message::Data { id, data } => {
+                write!(f, "Message::Data id={}, bytes={}", id, data.len())
+            }
+            Message::CloseConnection { id } => {
+                write!(f, "Message::CloseConnection id={}", id)
+            }
+            Message::Shutdown { message } => {
+                write!(f, "Message::Shutdown message={:?}", message)
+            }
+        }
+    }
 }
